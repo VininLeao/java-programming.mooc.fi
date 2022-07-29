ff15764d554592a7d029fadb690c39c296676eed
VininLeao <vininfleao@hotmail.com>
2022-07-28 21:01:01 -0300

    Part 1 of Java Programming 1 complete

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter a string");
+        String input = reader.nextLine();
+
+        if (input.equals("a string")) {
+            System.out.println("Great! You read the instructions correctly.");
+        } else {
+            System.out.println("Missed the mark!");
+        }
+    }
+}
